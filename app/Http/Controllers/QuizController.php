81e16ed6879a7bf1f8bb436d2e795744f9d07d8d
Cybernetics <mail@cybernetics.me>
2024-10-23 14:11:50 +0530

    small updates

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -105,9 +105,12 @@ class QuizController extends Controller
         $questions = DB::table('clarity_questions')->where('outcome', $outcome->id)->get();
         $focus = Outcome::where('category', $quiz->category)->where('outcome', $quiz->outcome)->first();
         $data = ['name' => $quiz->name];
-        Mail::to($quiz->email)->send(new ReportEmail($data));
-        echo "success";
-        /*$chart = $this->generateChart($quiz);
+        Mail::send('report-email', $data, function ($message) use ($quiz) {
+            $message->to($quiz->email, $quiz->name);
+            $message->subject('Life Style Design Quiz - Report');
+        });
+        /*Mail::to($quiz->email)->send(new ReportEmail($data));
+        $chart = $this->generateChart($quiz);
         $chart = base64_encode(file_get_contents($chart));
         $pdf = PDF::loadView('report', compact('quiz', 'strength', 'chart', 'questions', 'outcome', 'strengths', 'focus'));
         return $pdf->stream($quiz->id . '.pdf');
