236880a586da753b74dd3c989c466698eeb7814c
Cybernetics <mail@cybernetics.me>
2024-11-02 08:13:08 +0530

    small updates

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,7 +2,7 @@ $(function(){
     "use strict"
 
     $('form').submit(function() {
-        $(this).find(".btn-finish").attr("disabled", true);
+        /*$(this).find(".btn-finish").attr("disabled", true);*/
         $(this).find(".btn-finish").html("Submitting...<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>");
     });
 
