c56b2ef3ad3fcb66022e2f63430f7c9e344cf062
Cybernetics <mail@cybernetics.me>
2024-11-12 08:26:55 +0530

    small updates

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['web'])->group(function () {
     Route::prefix('')->controller(QuizController::class)->group(function () {
         Route::get('/', 'index')->name('index');
-        Route::get('/quiz', 'form')->name('form');
+        Route::get('/quiz', 'form')->name('quiz');
         Route::post('/quiz', 'submit')->name('submit');
         Route::get('/report/{id}', 'report')->name('report');
         Route::get('/thankyou', function () {
