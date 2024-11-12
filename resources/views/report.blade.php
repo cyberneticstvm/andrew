932cf39408b3f31e58aac2d30b4c31ba242fd41a
Cybernetics <mail@cybernetics.me>
2024-11-12 16:55:42 +0530

    small updates

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
     <style>
         @font-face {
             font-family: 'text';
