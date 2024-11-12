572d3e19085e3886cd11d652440d3aed6e88031a
Cybernetics <mail@cybernetics.me>
2024-11-12 16:59:08 +0530

    small updates

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -233,7 +233,7 @@
             <img src="data:image/png;base64, {{ $chart }}" />
         </div>
         <div class="col-md-12 text-center legend">
-            <span style="background-color: #ff0066;">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;Compassion&nbsp;&nbsp;&nbsp;&nbsp;
+            <span style="background-color: #ff0066; font-family: text">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;Compassion&nbsp;&nbsp;&nbsp;&nbsp;
             <span style="background-color: #f79109">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;Innovation&nbsp;&nbsp;&nbsp;&nbsp;
             <span style="background-color: #ffcc00">&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;Optimism&nbsp;&nbsp;&nbsp;&nbsp;
             <span style="background-color: #a8d18d">&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;Vision&nbsp;&nbsp;&nbsp;&nbsp;
